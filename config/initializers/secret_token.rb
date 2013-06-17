aabc104245e35f6818fc04b281a25f7da8a43533
KingMob <gsotero@gmail.com>
2013-06-16 21:59:52 -0300

    comecou

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jardim194::Application.config.secret_token = '9c428ed1b0c3d4f7bfeb7be1aba810565f11a16e029579dfbed57d1c184f8007edde78a85d95531852c81fce139bd585cab290ba3f806119ff8fd92304bbbe55'
